8151c2b31250d3fb74942311f2062745e742fbe7
Agnieszka Korczak <korczak.a.m@gmail.com>
2019-02-04 17:35:45 +0100

    Add project content

---
+++ App.js
@@ -0,0 +1,34 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '3464',
+  'X-Auth-Token': 'f57ba5b187d8f4406d350d3475aafead'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(cardObj);
+  	});
+}
+
+
